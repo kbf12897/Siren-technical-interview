dae7a0f66f01e50db3d041ee9e1e31456a27c5cd
Karl Felter <kbf12897@gmail.com>
2022-06-09 11:11:40 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const users = {
+    Karl: {
+        'favoriteMovie': 'Django Unchained',
+        'friends': [Rob, Mike, Allie]
+    },
+    Rob: {
+        'favoriteMovie': 'Interstellar',
+        'friends': [Shannon, Dante, Allie]
+    },
+    Dante: {
+        'favoriteMovie': 'Malignant',
+        'friends': [Mike, Nick, Shannon]
+     },
+    Allie: {
+        'favoriteMovie': 'Malignant',
+        'friends': [Karl, Rob, Mike]
+    },
+    Mike: {
+        'favoriteMovie': 'Django Unchained',
+        'friends': [Allie, Karl, Dante]
+    },
+    Shannon: {
+        'favoriteMovie': 'Interstellar',
+        'friends': [Rob, Dante, Nick]
+    },
+    Nick: {
+        'favoriteMovie': 'Django Unchained',
+        'friends': [Shannon, Dante]
+    }
+}
